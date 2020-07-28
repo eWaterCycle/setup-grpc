8fefd44bbc0bbeabb8eac60b9d3152ecc8f35ff7
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-07-28 18:28:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# GitHub Action to setup gRPC C++ environment
+
+![CI](https://github.com/eWaterCycle/setup-grpc/workflows/build-test/badge.svg)
+![Validate versions](https://github.com/eWaterCycle/setup-grpc/workflows/Validate%20'setup-grpc'/badge.svg)
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3964180.svg)](https://doi.org/10.5281/zenodo.3964180)
+
+To compile C++ code against gRPC in a workflow you need to install it first. This GitHub action compiles and installs it for you.
+
+## Inputs
+
+### `grpc-version`
+
+Version of grpc. See [releases page](https://github.com/hpcng/grpc/releases) for available versions. Versions lower then 3.6 need additional OS packages installed like `uuid-dev`.
+
+## Example usage
+
+```yaml
+steps:
+- uses: actions/checkout@v2
+- uses: eWaterCycle/setup-grpc@v1
+  with:
+    grpc-version: 1.27.2
+```
+
+## Build
+
+For developers of setup-grpc action.
+
+Install deps with
+
+```bash
+npm install
+```
+
+Build dist with
+
+```bash
+npm run build
+```
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import path from "path";
+import { cpus } from "os";
+
+import { info, getInput, setFailed } from "@actions/core";
+import { exec } from "@actions/exec";
+import { mkdirP } from "@actions/io";
+
+async function installGrpcVersion(versionSpec: string) {
+  info("Cloning grpc repo...");
+  await exec("git", [
+    "clone",
+    "--depth",
+    "1",
+    "--recurse-submodules",
+    "-b",
+    "v" + versionSpec,
+    "https://github.com/grpc/grpc",
+  ]);
+
+  const extPath = "grpc";
+  info(`Configuring in ${extPath}`);
+  const buildDir = path.join(extPath, "cmake", "build");
+  await mkdirP(buildDir);
+  // TODO Install into tool-cache with output/envars for later cmake with CMAKE_PREFIX_PATH
+  const prefixDir = "/usr/local";
+  await exec(
+    "cmake",
+    [
+      "-DgRPC_INSTALL=ON",
+      "-DgRPC_SSL_PROVIDER=package",
+      "-DgRPC_BUILD_TESTS=OFF",
+      "-DBUILD_SHARED_LIBS=ON",
+      `-DCMAKE_INSTALL_PREFIX=${prefixDir}`,
+      "../..",
+    ],
+    { cwd: buildDir }
+  );
+
+  info(`Compiling in ${buildDir}`);
+  const jn = cpus().length.toString();
+  await exec("make", ["-j", jn], { cwd: buildDir });
+
+  info(`Installing to ${prefixDir}`);
+  await exec("sudo make install", [], { cwd: buildDir });
+  await exec("sudo ldconfig", []);
+
+  return prefixDir;
+}
+
+async function main() {
+  const versionSpec = getInput("grpc-version");
+  info(`Setup grpc version spec ${versionSpec}`);
+
+  await installGrpcVersion(versionSpec);
+  info(`Successfully setup grpc version ${versionSpec}`);
+}
+
+main()
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((err) => {
+    setFailed(err.message);
+  });
